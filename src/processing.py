e432e5d6d6ab7ac02a2f2fe5666c0d0dbd8b39c6
sasha chernikova <chernikova.sahsa@yandex.ru>
2024-11-19 14:16:39 +0300

    message

--- src/processing.py
+++ src/processing.py
@@ -1,32 +1,20 @@
-from typing import Any
-
-list_of_dicts = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+from datetime import datetime
 
 
-def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
-    """
-    Функция принимает на вход список словарей и значение для ключа и возвращает новый
-    список содержащий только те словари у которых ключ содержит переданное в функцию
-    значение.
-    """
-    return [d for d in list_of_dict if d.get("state") == state]
+def filter_by_state(dict_list: list, state_value: str = "EXECUTED") -> list:
+    """Принимает список словарей и опционально значение для ключа state. Возвращает новый список словарей, содержащий
+    только те, у которых ключ state соответствует указанному значению (по умолчанию 'EXECUTED')"""
+    returned_list = []
+    for dict_ in dict_list:
+        if dict_["state"] == state_value:
+            returned_list.append(dict_)
+    return returned_list
 
 
-def sort_by_date(
-    list_of_dict: list[dict[str, Any]], reverse: bool = True
-) -> list[dict[str, Any]]:
-    """
-    Функция принимает на вход список словарей и возвращает новый список, в котором исходные
-    словари отсортированы по убыванию даты
-    """
+def sort_by_date(dict_list: list, sort_by_date_descending: bool = True) -> list:
+    """Принимает список словарей. Возвращает новый список, отсортированный по дате от новых к старым. Если надо
+    изменить порядок сортировки, то при вызове функции вторым параметром передай False"""
     sorted_list = sorted(
-        list_of_dict,
-        key=lambda new_list_of_dict: new_list_of_dict["date"],
-        reverse=reverse,
+        dict_list, key=lambda strindate: datetime.fromisoformat(strindate["date"]), reverse=sort_by_date_descending
     )
     return sorted_list
