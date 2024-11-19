b3bd8c6d58a1218285c06aa4f59cb2b18c3d4b90
sasha chernikova <chernikova.sahsa@yandex.ru>
2024-11-19 15:02:30 +0300

    init commit

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -23,29 +23,6 @@ def test_get_mask_card_number_minnumber(numbers_13):
     assert get_mask_card_number(numbers_13) == "7000 79** **** 9606"
 
 
-@pytest.mark.parametrize(
-    "value, expected",
-    [
-        ("70007922896063611234", "некорректный номер карты"),
-        ("700079228960", "некорректный номер карты"),
-        ("тестовые_буквы", "некорректный номер карты"),
-        ("a1b2c3d4e5f6g7h8i9j0", "некорректный номер карты"),
-        ("", "некорректный номер карты"),
-    ],
-)
-def test_get_mask_card_number_valueerror(value, expected):
-    with pytest.raises(ValueError):
-        get_mask_card_number(value) == expected
-
-
-@pytest.mark.parametrize(
-    "value, expected",
-    [
-        ("70007922896063611234", "**1234"),
-        ("70007 92289 60636 11234", "**1234"),
-        (70007922896063611234, "**1234"),
-    ],
-)
 def test_get_mask_account(numbers_20):
     assert get_mask_account(numbers_20) == "**1234"
 
@@ -57,18 +34,3 @@ def test_get_mask_account_with_spaces(numbers_20_spases):
 def test_get_mask_account_int(numbers_20_int):
     assert get_mask_account(numbers_20_int) == "**1234"
 
-
-@pytest.mark.parametrize(
-    "value, expected",
-    [
-        ("7000792289606361", "некорректный номер счета"),
-        ("7000792289606361123", "некорректный номер счета"),
-        ("тестовые_буквы", "некорректный номер счета"),
-        ("a1b2c3d4e5f6g7h8i9j0", "некорректный номер счета"),
-        ("", "некорректный номер счета"),
-        ("700079228960636112345", "некорректный номер счета"),
-    ],
-)
-def test_get_mask_account_valueerror(value, expected):
-    with pytest.raises(ValueError):
-        get_mask_account(value) == expected
