0bbfb3dc9fd3b604096a2d131a68cd55540b2ab5
sasha chernikova <chernikova.sahsa@yandex.ru>
2024-12-23 19:47:45 +0300

    init commit

--- src/external_api.py
+++ src/external_api.py
@@ -7,9 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 payload = {}
-headers = {
-    "apikey": os.getenv("API_KEY")
-}
+headers = {"apikey": os.getenv("API_KEY")}
 
 
 def converter_into_rubles(transaction: dict) -> float:
@@ -17,11 +15,15 @@ def converter_into_rubles(transaction: dict) -> float:
     RUB, USD, EUR"""
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
         return float(transaction["operationAmount"]["amount"])
-    elif (transaction["operationAmount"]["currency"]["code"] == "USD" or
-          transaction["operationAmount"]["currency"]["code"] == "EUR"):
-        url = (f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from="
-               f"{transaction["operationAmount"]["currency"]["code"]}&amount="
-               f"{transaction["operationAmount"]["amount"]}")
+    elif (
+        transaction["operationAmount"]["currency"]["code"] == "USD"
+        or transaction["operationAmount"]["currency"]["code"] == "EUR"
+    ):
+        url = (
+            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from="
+            f"{transaction["operationAmount"]["currency"]["code"]}&amount="
+            f"{transaction["operationAmount"]["amount"]}"
+        )
         response = requests.request("GET", url, headers=headers, data=payload)
         result = response.text
         result = json.loads(result)
--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,25 @@
+import logging
+import os
 from typing import Any
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(f"{os.path.join(os.path.dirname(__file__), os.pardir)}\\logs\\masks.log")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_mask_card_number(card_number: Any) -> str:
     """Принимает на вход номер карты, возвращает маску карты в типе str"""
     card_number = str(card_number)
     card_number = card_number.replace(" ", "")
+    logger.info(f"Маскируем карту с номером {card_number}")
     if card_number.isdigit() and 12 < len(card_number) < 20:
         masked_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+        logger.info("Номер карты замаскирован")
         return str(masked_card_number)
+    logger.error(f"введен некорректный номер карты - {card_number}")
     raise ValueError("некорректный номер карты")
 
 
@@ -15,7 +27,10 @@ def get_mask_account(account_number: Any) -> str:
     """Принимет номер счета, возвращает маску счета в типе str"""
     account_number = str(account_number)
     account_number = account_number.replace(" ", "")
+    logger.info(f"Маскируем счет с номером {account_number}")
     if account_number.isdigit() and len(account_number) == 20:
         masked_account = "**" + account_number[-4:]
+        logger.info("Номер счета замаскирован")
         return str(masked_account)
+    logger.error(f"введен некорректный номер счета - {account_number}")
     raise ValueError("некорректный номер счета")
