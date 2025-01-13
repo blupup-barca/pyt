a70a1de30bc62c0670c4b51fa44d6d92662a3285
sasha chernikova <chernikova.sahsa@yandex.ru>
2025-01-13 13:30:52 +0300

    init commit

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+import re
+from collections import Counter
 from datetime import datetime
 
 
@@ -18,3 +20,27 @@ def sort_by_date(dict_list: list, sort_by_date_descending: bool = True) -> list:
         dict_list, key=lambda strindate: datetime.fromisoformat(strindate["date"]), reverse=sort_by_date_descending
     )
     return sorted_list
+
+
+def filter_by_description(transaction_list: list[dict], search_value: str) -> list[dict]:
+    """Принимает список словарей и строковое значение. Возвращает список словарей, у которых ключ description
+    соответствует строке из второго аргумента (в том числе частично)"""
+    returned_list = []
+    for transaction in transaction_list:
+        if re.search(search_value, transaction["description"], flags=re.IGNORECASE):
+            returned_list.append(transaction)
+    return returned_list
+
+
+def counter_by_description(transaction_list: list[dict], description_list: list[str]) -> dict:
+    """Принимает на вход список словарей с данными о банковских операциях и список категорий операций.
+    Возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций
+    в каждой категории. Если категория была в передаваемом списке категорий операций, а в возвращенном словаре
+    ее нет - значит, количество операций с такой категорией равно 0"""
+    list_for_requirement = []
+    for description in description_list:
+        for transaction in transaction_list:
+            if transaction["description"] == description.title():
+                list_for_requirement.append(transaction["description"])
+    returned_dict = Counter(list_for_requirement)
+    return returned_dict
